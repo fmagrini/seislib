1b7ccb79b4afa83298a0ff1157b6de90d3555fd8
Fabrizio Magrini <98057074+fmagrini@users.noreply.github.com>
2023-09-06 11:27:32 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -56,13 +56,18 @@ def setup_cython_extension():
 	extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ]
 	platform_name = platform.system()
 	if platform_name.lower() == 'darwin':
-		versions = os.listdir('/usr/local/Cellar/gcc/')
-		version = max(versions, key=lambda i: int(i.split('.')[0]))
-		version_int = version.split('.')[0]
-		path = '/usr/local/Cellar/gcc/%s/lib/gcc/%s'%(version, version_int)
-		os.environ['CC'] = 'gcc-%s'%version_int
-		os.environ['CXX'] = 'g++-%s'%version_int
-		extra_link_args=['-Wl,-rpath,%s'%path]
+	        src_paths = ['/usr/local', '/opt/homebrew']
+                for src in src_paths:
+                    gcc_path = os.path.join(src, 'Cellar/gcc')
+                    if not os.path.exists(gcc_path):
+                        continue
+                    versions = os.listdir(gcc_path)
+                    version = max(versions, key=lambda i: int(i.split('.')[0]))
+                    version_int = version.split('.')[0]
+                    path = os.path.join(gcc_path, '%s/lib/gcc/%s'%(version, version_int))
+                    os.environ['CC'] = 'gcc-%s'%version_int
+                    os.environ['CXX'] = 'g++-%s'%version_int
+                    extra_link_args=['-Wl,-rpath,%s'%path]
 	else:
 		extra_link_args=['-fopenmp']
 	ext_modules = []
