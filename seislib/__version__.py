57e825752e6f90466d5abc5ef1c98412499c34a9
Fabrizio Magrini <98057074+fmagrini@users.noreply.github.com>
2024-11-19 14:56:47 +1100

    version

--- seislib/__version__.py
+++ seislib/__version__.py
@@ -1,6 +1,6 @@
 __major_version__  = 0
-__minor_version__  = 6
-__patch__          = 27
+__minor_version__  = 7
+__patch__          = 1
 __version_tuple__ = (
     __major_version__,
     __minor_version__,
